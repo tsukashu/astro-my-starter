b38cb2f2cb9a07f7f0fb6f2ea882b0a0dd0e1c14
tsukashu <80503344+tsukashu@users.noreply.github.com>
2026-03-18 19:10:22 +0900

    chore: simplify browserslist and postcss config with baseline support
    - browserslistrcを`supports baseline-*`ベースに変更し、production/developmentで適切な安定性レベルを設定
    - postcss.configから不要なコメントとmedia-query-rangesプラグインを削除し、設定を簡素化

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,14 @@
-// TODO: postCSS と SCSSを スムーズに切り替え、あるいは使い分けられるような構成にしたい
-
-// import postcssGlobalData from "@csstools/postcss-global-data";
 import postcssPresetEnv from "postcss-preset-env";
 
 export default {
   plugins: [
-    // Note: postcssGlobalData is placed before postcssPresetEnv
-    // postcssGlobalData({
-    // files: ["./src/styles/custom-media.css"],
-    // }),
     postcssPresetEnv({
-      stage: 3, // used for add prefixes
-      minimumVendorImplementations: 2, // for better stability
+      stage: 3,
+      minimumVendorImplementations: 2,
       features: {
-        // must plugins, or if all of them are implemented enough, maybe no more to use postcss-preset-env.
-        "media-query-ranges": true,
         "custom-media-queries": true,
       },
-      autoprefixer: true, // explicitly
+      autoprefixer: true,
     }),
   ],
 };
